699368166cbfdbae5cbe9b11e812367391f089c1
Matthew Lee <matthewjlee256@gmail.com>
2016-11-21 20:07:09 -0500

    more additions

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongo = require('mongodb').MongoClient
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
+//make sure to place body-parser before CRUD-handlers!
+
+app.get('/', (req, res) => {
+	res.sendFile('/Users/mjlee/Documents/learnnode/ExpressTutorial' + '/index.html')
+})
+
+//navigating to /quotes
+app.post('/quotes', (req, res) => {
+	console.log(req.body);
+})
+
+mongoconnect('link-to-mongoddb,' (err, database)) => {
+	//start the server
+}
 
 app.listen(3000, function() {
 	console.log('listening on 3000')
